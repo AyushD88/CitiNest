d562df90ce0addfd10cd50654135704aab7def27
Ayush Dabhi <ayushdabhi212@gmail.com>
2024-08-22 14:56:54 +0530

    complete sign in Page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -71,13 +71,13 @@ export default function SignUp() {
           {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
-      <div className="flex gap-2 mt-5">
-        <p>Have an accout?</p>
-        <Link to={"/sign-in"}>
-          <span className="text-blue-700">Sign In</span>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      {error && <p className="text-red-500 mt-5">{error}</p>}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 }
